04fe60147e5c0dc13408e49da4928f86c059f46a
Rafaelrfsilva <rrodriguesfs@gmail.com>
2025-10-27 18:42:38 -0300

    Corrige imports do authOptions em todos os arquivos

--- app/api/enviar-relatorio/route.ts
+++ app/api/enviar-relatorio/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/authOptions";
 import { NextResponse } from "next/server";
 import { sendEmail } from "@/lib/email";
 
